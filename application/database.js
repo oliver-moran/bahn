3e6a96ac0b5d18d0ed1f6e80699be0bbf0ece279
Oliver Moran <oliver.moran@gmail.com>
2014-07-16 00:41:07 +0100

    add mongodb support (basic and untested)

--- application/database.js
+++ application/database.js
@@ -1,9 +1,18 @@
 var NeDB = require('nedb');
+// var MongoClient = require('mongodb').MongoClient;
 
 function Database () {
     /* CREATE NEW DATABASES HERE */
+    
     this.tasks = new NeDB({ filename: './application/data/tasks.db' });
     this.tasks.loadDatabase();
+    
+    /*
+    // MongoDB equivilent
+    MongoClient.connect('mongodb://127.0.0.1:27017/bahn?w=0', (function(err, db) {
+        this.tasks = db.collection("tasks");
+    }).bind(this));
+    */
 };
 
 module.exports = Database;
