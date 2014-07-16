8724dd47ce257156e8c9fe44fe1dcd25502253b1
Oliver Moran <oliver.moran@gmail.com>
2014-07-17 00:29:32 +0100

    improve comments and docs, use UUID

--- application/socket.js
+++ application/socket.js
@@ -1,5 +1,11 @@
+var SocketIO = require("socket.io");
+
+/**
+ * Constructs a Socket object
+ */
 function Socket(server) {
-    this.io = require("socket.io").listen(server);
+    // expose the listening socket 
+    this.io = SocketIO.listen(server);
     
     /* ADD NEW SOCKET EVENT LISTENERS HERE */
     this.io.on("connection", function (socket) {
@@ -9,24 +15,28 @@ function Socket(server) {
     });
 }
 
+// the listening SocketIO object
 Socket.prototype.io = null;
 
 /* HANDLE SOCKET EVENTS HERE */
 
+var UUID = require("node-uuid");
+
 function insertTask(task) {
-    // padding task into the db appends _id to it
+    task.uuid = UUID.v4(); // add a UUID that we will use to ID the document
     bahn.database.tasks.insert(task);
     bahn.socket.io.sockets.emit("insertTask", task);
 }
 
 function updateTask(task) {
-    bahn.database.tasks.update({ _id: task._id }, { $set: { status: task.status } });
+    bahn.database.tasks.update({ uuid: task.uuid }, { $set: { status: task.status } });
     bahn.socket.io.sockets.emit("updateTask", task);
 }
 
 function removeTask(task) {
-    bahn.database.tasks.remove({ _id: task._id });
+    bahn.database.tasks.remove({ uuid: task.uuid });
     bahn.socket.io.sockets.emit("removeTask", task);
 }
 
+// export Socket for use in bahn
 module.exports = Socket;
